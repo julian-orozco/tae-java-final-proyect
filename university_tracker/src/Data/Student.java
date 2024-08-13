1dfc2cbe118194bc6430a8b272ec745affa9dffc
Julian Orozco Vanegas <julian.orozco@globant.com>
2024-08-13 09:20:17 -0500

    Created contructor for student class

--- university_tracker/src/Data/Student.java
+++ university_tracker/src/Data/Student.java
@@ -4,7 +4,13 @@ public class Student {
     private String name;
     private int id;
     private int age;
-    
+
+    public Student(String name, int id, int age){
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
